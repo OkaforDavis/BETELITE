a24640fd3d67e8d4e28b40995d3f658710fa8838
OkaforDavis <okafordavis@github.com>
2026-04-27 21:13:52 +0000

    Add firebase serve to test environment startup

--- scripts/start-test-env.bat
+++ scripts/start-test-env.bat
@@ -11,3 +11,8 @@ echo Both services are starting up.
 echo - Node.js Backend: http://localhost:3000
 echo - Python Detection API: http://localhost:5000
 echo - Mobile Frontend: http://localhost:3000/mobile
+
+echo Starting Firebase Hosting Emulator...
+start cmd /k "firebase serve --port 5005"
+echo - Firebase Frontend: http://localhost:5005
+
